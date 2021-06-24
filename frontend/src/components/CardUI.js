e73eb9ecdd467caad273de0cb7efe1baf56f4154
LuisBiceps <reyesrosadoluis1@gmail.com>
2021-06-24 16:48:13 -0400

    first

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -1,113 +1,137 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function CardUI()
 {
-    var card = '';
-    var search = '';
-    
-    const [message,setMessage] = useState('');
-    const [searchResults,setResults] = useState('');
-    const [cardList,setCardList] = useState('');
 
-   var _ud = localStorage.getItem('user_data');
+    var bp = require('./Path.js');
+    var storage = require('../tokenStorage.js');
+    const jwt = require("jsonwebtoken");
+    
+    var card = '';
+    var search = '';
+
+    const [message,setMessage] = useState('');
+    const [searchResults,setResults] = useState('');
+    const [cardList,setCardList] = useState('');
+
+    var _ud = localStorage.getItem('user_data');
     var ud = JSON.parse(_ud);
-    var userId = ud.id;
+    var userId = ud.userId;
     var firstName = ud.firstName;
     var lastName = ud.lastName;
 	
-    const app_name = 'meetadime'
-    function buildPath(route){    
-        if (process.env.NODE_ENV === 'production')     
+    const addCard = async event => 
+    {
+	    event.preventDefault();
+
+        var tok = storage.retrieveToken();
+       var obj = {userId:userId,card:card.value,jwtToken:tok};
+       var js = JSON.stringify(obj);
+
+        var config = 
         {
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
-        }
-        else
-        {
-            return 'http://localhost:5000/' + route;    
-        }
-    }
+            method: 'post',
+            url: bp.buildPath('api/addcard'),	
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            },
+            data: js
+        };
     
-    const addCard = async event => 
-    {
-	    event.preventDefault();
-
-        var obj = {userId:userId,card:card.value};
-        var js = JSON.stringify(obj);
-
-        try
-        {
-            const response = await fetch(buildPath("api/addcard"),
-            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-
-            var txt = await response.text();
-            var res = JSON.parse(txt);
-
-            if( res.error.length > 0 )
-            {
-                setMessage( "API Error:" + res.error );
-            }
-            else
-            {
-                setMessage('Card has been added');
-            }
-        }
-        catch(e)
-        {
-            setMessage(e.toString());
-        }
+        axios(config)
+            .then(function (response) 
+        {
+            var res = response.data;
+            var retTok = res.jwtToken;
+    
+            if( res.error.length > 0 )
+            {
+                setMessage( "API Error:" + res.error );
+            }
+            else
+            {
+                setMessage('Card has been added');
+                storage.storeToken( {accessToken:retTok} );
+            }
+        })
+        .catch(function (error) 
+        {
+            console.log(error);
+        });
 
 	};
 
-    const searchCard = async event => 
-    {
-        event.preventDefault();
-        		
-        var obj = {id:userId,search:search.value};
+    const searchCard = async event => 
+    {
+        event.preventDefault();
+        		
+        var tok = storage.retrieveToken();
+        var obj = {userId:userId,search:search.value,jwtToken:tok};
         var js = JSON.stringify(obj);
 
-        try
-        {
-            const response = await fetch(buildPath("api/searchcards"),
-            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+        var config = 
+        {
+            method: 'post',
+            url: bp.buildPath('api/searchcards'),	
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            },
+            data: js
+        };
+    
+        axios(config)
+            .then(function (response) 
+        {
+            var res = response.data;
+            var retTok = res.jwtToken;
+    
+            if( res.error.length > 0 )
+            {
+                setMessage( "API Error:" + res.error );
+            }
+            else
+            {
+                var _results = res.results;
+                var resultText = '';
+                for( var i=0; i<_results.length; i++ )
+                {
+                    resultText += _results[i];
+                    if( i < _results.length - 1 )
+                    {
+                        resultText += ', ';
+                    }
+                }
+                setResults('Card(s) have been retrieved');
+                setCardList(resultText);
+                storage.storeToken( {accessToken:retTok} );
+            }
+        })
+        .catch(function (error) 
+        {
+            console.log(error);
+        });
 
-            var txt = await response.text();
-            var res = JSON.parse(txt);
-            var _results = res.results;
-            var resultText = '';
-            for( var i=0; i<_results.length; i++ )
-            {
-                resultText += _results[i];
-                if( i < _results.length - 1 )
-                {
-                    resultText += ', ';
-                }
-            }
-            setResults('Card(s) have been retrieved');
-            setCardList(resultText);
-        }
-        catch(e)
-        {
-            alert(e.toString());
-            setResults(e.toString());
-        }
-    };
+    };
 
-    return(
-          <div id="cardUIDiv">
-      <br />
-      <input type="text" id="searchText" placeholder="Card To Search For" 
-        ref={(c) => search = c} />
-      <button type="button" id="searchCardButton" className="buttons" 
-        onClick={searchCard}> Search Card</button><br />
-      <span id="cardSearchResult">{searchResults}</span>
-      <p id="cardList">{cardList}</p><br /><br />
-      <input type="text" id="cardText" placeholder="Card To Add" 
-        ref={(c) => card = c} />
-      <button type="button" id="addCardButton" className="buttons" 
-        onClick={addCard}> Add Card </button><br />
-      <span id="cardAddResult">{message}</span>
-    </div>
-    );
+    return(
+        <div id="cardUIDiv">
+        <br />
+        <input type="text" id="searchText" placeholder="Card To Search For" 
+            ref={(c) => search = c} />
+        <button type="button" id="searchCardButton" class="buttons" 
+            onClick={searchCard}> Search Card</button><br />
+        <span id="cardSearchResult">{searchResults}</span>
+        <p id="cardList">{cardList}</p><br /><br />
+        <input type="text" id="cardText" placeholder="Card To Add" 
+            ref={(c) => card = c} />
+        <button type="button" id="addCardButton" class="buttons" 
+            onClick={addCard}> Add Card </button><br />
+        <span id="cardAddResult">{message}</span>
+        </div>
+    );
 }
 
 export default CardUI;
--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -3,37 +3,28 @@ import React from 'react';
 function LoggedInName()
 {
 
-    try {
-        var _ud = localStorage.getItem('user_data');
-        var ud = JSON.parse(_ud);
-        var userId = ud.id;
-        var firstName = ud.firstName;
-        var lastName = ud.lastName;
-    
-    } catch (error) {
-        // console.log(error);
+    var _ud = localStorage.getItem('user_data');
+    var ud = JSON.parse(_ud);
+    var userId = ud.id;
+    var firstName = ud.firstName;
+    var lastName = ud.lastName;
+
+    const doLogout = event => 
+    {
+	    event.preventDefault();
+
+        localStorage.removeItem("user_data")
         window.location.href = '/';
-        return;
-    }
-    
 
-    const doLogout = event => 
-    {
-	    event.preventDefault();
+    };    
 
-        localStorage.removeItem("user_data")
-        window.location.href = '/';
-
-    };    
-
-  return(
-   <div id="loggedInDiv">
+  return(
+   <div id="loggedInDiv">
    <span id="userName">Logged In As {firstName} {lastName}</span><br />
-   <button type="button" id="logoutButton" className="buttons" 
+   <button type="button" id="logoutButton" class="buttons" 
      onClick={doLogout}> Log Out </button>
-   </div>
-  );
-
+   </div>
+  );
 };
 
 export default LoggedInName;
