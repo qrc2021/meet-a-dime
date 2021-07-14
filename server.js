3acf9e3d9682ea7ec60ded7d133b853048437738
Luis <luivader2000@gmail.com>
2021-07-14 12:24:50 -0400

    server js fix

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ admin.initializeApp({
 });
 
 async function decodeIDToken(req, res, next) {
-  if (req.headers?.authorization?.startsWith("Bearer ")) {
+  if (
+    req &&
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
     const idToken = req.headers.authorization.split("Bearer ")[1];
 
     try {
